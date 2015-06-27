f05f2601dcb71677bd16f7bd13ba03dcccc86447
NurIchsan81 <nur.ichsan.amin@gmail.com>
2015-06-28 01:51:32 +0800

    Create Laporan2.java

---
+++ Laporan2.java
@@ -0,0 +1,36 @@
+
+public class Laporan2 {
+	static boolean blean;
+	static long lo;
+	static String \u0073\u0074\u0072;
+	static int in;
+	static float flo;
+	
+	public static void main (String args[]) {
+		if (in==0) {
+			Boolean refBoolean = new Boolean(true);
+			boolean bool = refBoolean.booleanValue();
+			System.out.println("nilai boolean = " +bool);
+			
+			Long refloLong = new Long(100000000);
+			long loo = refloLong.longValue();
+			Float refflFloat = new Float(12.3F);
+			float flo = refflFloat.floatValue();
+			
+			System.out.println("nilai float = " +flo);
+			System.out.println("nilai long = " +loo);
+			str = "Ichsan";
+			System.out.print(str);
+			}
+		else {
+			System.out.println("nilai integer = " +in);
+			System.out.println("nilai boolean = " +blean);
+			System.out.println("nilai float = " +flo);
+			System.out.println("nilai long = " +lo);
+			System.out.println("nilai string = " +str);
+		}
+	}
+	
+	
+
+}
